eca7cb5046a369b709f10c9f84f36dba3a1bf65e
Agustin Martina <amartina@unc.edu.ar>
2020-06-01 22:47:39 -0300

    updated downald GFS file

--- descarga_GFS025.py
+++ descarga_GFS025.py
@@ -6,8 +6,6 @@
 
 # Attention: trailing spaces are mandatory in structured "if" or "for"
 
-
-import sys
 # for file-directory functions
 import os
 # for sleep command
@@ -17,7 +15,44 @@ import os.path
 import requests
 import datetime
 import socket
-from optparse import OptionParser
+import argparse
+
+# CONFIGURATION OF GRIB FILTER AND DATE RANGE
+# Domain (limited area) definition (geographical coordinates)
+LON_W = "-96"
+LON_E = "-15"
+LAT_N = "-10"
+LAT_S = "-75"
+
+# Data grid resolution (in degree)
+ADGRID = "0.25"  # can be:0.25, 0.5, 1.0, 2.5
+
+# Total forecast length (in hours) for which data are requested:
+NHOUR = int(os.environ['RUN_HOURS'])
+# Interval in hours between two forecast times:
+DHOUR = 3
+
+# Count files to request
+CANT_FILES_REQUESTED = NHOUR/DHOUR+1
+
+COUNTMAX = 50
+ICOUNTMAX = 100
+S_SLEEP1 = 600
+S_SLEEP2 = 60
+
+# Definition of requested levels and parameters
+# PAR_LIST=["TMAX"]
+PAR_LIST = ["HGT", "LAND", "PRES", "PRMSL", "RH", "SOILW", "SPFH", "TMP",
+            "UGRD", "VGRD", "WEASD", "TSOIL"]
+
+# ADDR
+BASE_ADDR = "https://nomads.ncep.noaa.gov/cgi-bin/"
+
+PERL_FILTER = "filter_gfs_0p25.pl"
+
+DOMAIN_PARAMETERS = (f"&subregion=&leftlon={LON_W}&rightlon={LON_E}"
+                     f"&bottomlat={LAT_S}&toplat={LAT_N}")
+LEVELS = "&all_lev=on"
 
 
 def download_grb_file(url, outfile):
@@ -40,7 +75,7 @@ def download_grb_file(url, outfile):
         print('Failed to download gfs data for date')
         return -1
 
-    # logging.info('Start download of {:%Y-%m-%d %H:%M}'.format(date))
+    # logging.info('Start download DOMAIN_PARAMETERScontent(1024):
     with open(outfile, 'wb') as f:
         for block in response.iter_content(1024):
             f.write(block)
@@ -49,7 +84,76 @@ def download_grb_file(url, outfile):
     return 0
 
 
-def download(inidate, output_dir):
+def get_list_gfs(inidate):
+    inidate = int(inidate)      # CAST INIT date
+    # inidate=int(os.environ['inidate'])
+
+    # Date of forecast start (analysis)
+    date = inidate//100
+    # Instant (hour, UTC) of forecast start (analysis)
+
+    # fci=int(sys.argv[1]) # reads input on the calling line
+    # (example: "python get_GFS_grib2_slice.py 00")
+    # fci=input('Type UTC time of analysis (00, 06, 12, 18) ')
+    # # for request of manual input after launch
+    fci = inidate-date*100
+
+    # Defines connection timeoutcd
+    socket.setdefaulttimeout(30)
+
+    # Definiton of date (in required format)
+    day = datetime.datetime.today()
+    # tomorrow=day+datetime.timedelta(days=1)
+    # yesterday=day+datetime.timedelta(days=-1)
+
+    # If the download is made early in the morning,
+    # the date is that of yesterday
+
+    # if day.hour < 6 :
+    #   day=yesterday
+
+    # day="%4.4i%2.2i%2.2i" % (day.year,day.month,day.day)
+    # # structure definition
+    day = "%8.8i" % (date)
+    fciA = "%2.2i" % fci
+
+    print("Date and hour of GFS forecast initial time: ", day, fci)
+
+    dir_gfs_name = f"&dir=%2Fgfs.{day}%2F{fciA}"
+
+    # Full list of requested files
+    list_remote_files = []
+    list_files_local = []
+
+    for iter_num_file in range(0, int(CANT_FILES_REQUESTED)):
+
+        npar = len(PAR_LIST)
+        parameters = ""
+        for iparam in range(0, npar):
+            parameters = parameters + f"&var_{PAR_LIST[iparam]}=on"
+
+        hf = iter_num_file*DHOUR
+        hfA2 = "%3.3i" % hf
+
+        file_name_base = f"?file=gfs.t{fciA}z.pgrb2.0p25.f{hfA2}"
+
+        remote_file = (f"{PERL_FILTER}{file_name_base}{LEVELS}"
+                       f"{parameters}{DOMAIN_PARAMETERS}{dir_gfs_name}")
+        local_file = f"GFS_{day}{fciA}+{hfA2}.grib2"
+
+        list_remote_files.append(remote_file)
+        list_files_local.append(local_file)
+
+        print("********************************")
+        print(remote_file)
+        print("********************************")
+        print(local_file)
+        print("********************************")
+
+    return list_remote_files, list_files_local
+
+
+def download(output_dir, list_remote_files, list_files_local):
     """
     Se descargan los datos con los siguientes parámetros::
 
@@ -70,245 +174,101 @@ def download(inidate, output_dir):
 
     """
 
-    inidate = int(inidate)      # CAST INIT date
-    # inidate=int(os.environ['inidate'])
-
-    # Date of forecast start (analysis)
-    date = inidate//100
-    # Instant (hour, UTC) of forecast start (analysis)
-
-    # fci=int(sys.argv[1]) # reads input on the calling line
-    # (example: "python get_GFS_grib2_slice.py 00")
-    # fci=input('Type UTC time of analysis (00, 06, 12, 18) ')
-    # # for request of manual input after launch
-    fci = inidate-date*100
-
-    # Domain (limited area) definition (geographical coordinates)
-    LON_W = "-96"
-    LON_E = "-15"
-    LAT_N = "-10"
-    LAT_S = "-75"
-
-    # Data grid resolution (in degree)
-    ADGRID = "0.25"  # can be:0.25, 0.5, 1.0, 2.5
-
-    # Defines connection timeoutcd
-    socket.setdefaulttimeout(30)
-
-    # Total forecast length (in hours) for which data are requested:
-    NHOUR = int(os.environ['RUN_HOURS'])
-
-    # Interval in hours between two forecast times:
-    DHOUR = 3
-
-    # Definiton of date (in required format)
-    day = datetime.datetime.today()
-    # tomorrow=day+datetime.timedelta(days=1)
-    # yesterday=day+datetime.timedelta(days=-1)
-
-    # If the download is made early in the morning,
-    # the date is that of yesterday
-
-    # if day.hour < 6 :
-    #   day=yesterday
-
-    # day="%4.4i%2.2i%2.2i" % (day.year,day.month,day.day)
-    # # structure definition
-    day = "%8.8i" % (date)
-
-    fciA = "%2.2i" % fci
-
-    print("Date and hour of GFS forecast initial time: ", day, fci)
-
-    # Definition of requested levels and parameters
-    LEV_LIST = ["all"]
-    # PAR_LIST=["TMAX"]
-    PAR_LIST = ["HGT", "LAND", "PRES", "PRMSL", "RH", "SOILW", "SPFH", "TMP",
-                "UGRD", "VGRD", "WEASD", "TSOIL"]
-
-    COUNTMAX = 50
-    ICOUNTMAX = 100
-    S_SLEEP1 = 600
-    S_SLEEP2 = 60
-
-    # N_LEV_TYPE = 1
-    NINSTANT = NHOUR/DHOUR+1
-    NFILE_REQUESTED = [NINSTANT, 1]
-
-    FILE_NAME_DOMAIN = ("&subregion=&leftlon=" + LON_W + "&rightlon=" + LON_E
-                        + "&bottomlat=" + LAT_S + "&toplat=" + LAT_N)
-    if ADGRID == "0.25":
-        FILE_NAME_INI = "filter_gfs_0p25.pl"
-        DIR_NAME = "&dir=%2Fgfs." + day + "%2F" + fciA
-
-    if ADGRID == "0.5":
-        FILE_NAME_INI = "filter_gfs_hd.pl"
-        DIR_NAME = "&dir=%2Fgfs." + day + fciA + "%2Fmaster"
-
-    if ADGRID == "1.0":
-        FILE_NAME_INI = "filter_gfs.pl"
-        DIR_NAME = "&dir=%2Fgfs." + day + fciA
-
-    if ADGRID == "2.5":
-        FILE_NAME_INI = "filter_gfs_2p5.pl"
-        DIR_NAME = "&dir=%2Fgfs." + day + fciA
-
-    # Full list of requested files
-
-    LIST_FILE_REMOTE = []
-    # LIST_FILE_LOCAL = []
-    LIST_FLAG = []
-    LIST_FILE_LOCAL_FIN = []
-    NINST = NFILE_REQUESTED[0]
-    for INST in range(0, int(NINST)):
-        if INST+1 > NFILE_REQUESTED[0]:
-            continue
-        NLEV = len(LEV_LIST)
-        NPAR = len(PAR_LIST)
-        PARAMETERS = ""
-        for IPAR in range(0, NPAR):
-            PARAMETERS = PARAMETERS + "&var_" + PAR_LIST[IPAR] + "=on"
-            LEVELS = ""
-
-        if LEV_LIST[0] == "all":
-            LEVELS = LEVELS + "&all_lev=on"
-        else:
-            for ILEV in range(0, NLEV):
-                LEVELS = LEVELS + "&lev_" + LEV_LIST[ILEV] + "=on"
-            HF = INST*DHOUR
-            HFA = "%2.2i" % HF
-            HFA2 = "%3.3i" % HF
-
-            if ADGRID == "0.25":
-                FILE_NAME_BASE = f"?file=gfs.t{fciA}z.pgrb2.0p25.f{HFA2}"
-            if ADGRID == "0.5":
-                FILE_NAME_BASE = f"?file=gfs.t{fciA}z.mastergrb2f{HFA}"
-            if ADGRID == "1.0":
-                FILE_NAME_BASE = f"?file=gfs.t{fciA}z.pgrbf{HFA}.grib2"
-            if ADGRID == "2.5":
-                FILE_NAME_BASE = f"?file=gfs.t{fciA}z.pgrbf{HFA}.2p5deg.grib2"
-
-            FILE_REMOTE = (FILE_NAME_INI + FILE_NAME_BASE + LEVELS
-                           + PARAMETERS + FILE_NAME_DOMAIN + DIR_NAME)
-            # FILE_LOCAL="GFS_"+LEV_TYPE[ILEVTYPE]+"_"+day+fciA+"+"+HFA2+".grib2"
-            FILE_LOCAL_FIN = "GFS_" + day+fciA + "+" + HFA2 + ".grib2"
-            FLAG = 0
-            LIST_FILE_REMOTE.append(FILE_REMOTE)
-            # LIST_FILE_LOCAL.append(FILE_LOCAL)
-            # if ILEVTYPE==0: LIST_FILE_LOCAL_FIN.append(FILE_LOCAL_FIN)
-            LIST_FILE_LOCAL_FIN.append(FILE_LOCAL_FIN)
-            LIST_FLAG.append(FLAG)
-
-            print("********************************")
-            print(FILE_REMOTE)
-            print("********************************")
-            print(FILE_LOCAL_FIN)
-            print("********************************")
-
-    NFILE = len(LIST_FLAG)
-
     # sys.exit(0)
-
     # Dowloading of requested files
-
     WORK = True
-    while WORK:
-        NOMADS_ADDR = "https://nomads.ncep.noaa.gov/cgi-bin/"
-        print('Request in server: ', NOMADS_ADDR)
 
-        count = 1
-        while count <= COUNTMAX:
-            print('Attempt number: ', count)
-            count_req_files = 0
-            NFLAG = 0
-            for IFILE in range(0, NFILE):
-                    if LIST_FLAG[IFILE] == 0:
-                        NFLAG = NFLAG + 1
-                        FILE_REMOTE = NOMADS_ADDR+LIST_FILE_REMOTE[IFILE]
-                        FILE_LOCAL = (f"{output_dir}/"
-                                      f"{LIST_FILE_LOCAL_FIN[IFILE]}")
+    count_files_to_download = len(list_remote_files)
+    print(f"Request in server: {BASE_ADDR}")
+    print(f"count of files to download: {count_files_to_download}")
+    # extenar check, if this fail we go out
+    count = 1
 
-                        ##############################
-                        print("FILE_LOCAL" + FILE_LOCAL)
-                        print("FILE_REMOTE" + FILE_REMOTE)
-                        # sys.exit(0)
+    while count <= COUNTMAX:
+        print('Attempt number: ', count)
+        count_req_files = 0
+        # to check if all desired files were downloaded
+        count_down_iter = 0
 
-                        ############################
+        for ifile in range(0, count_files_to_download):
+            count_down_iter = count_down_iter + 1
+            remote_file = f"{BASE_ADDR}{list_remote_files[ifile]}"
+            local_file = (f"{output_dir}/{list_files_local[ifile]}")
 
-                        ierr = 100
-                        icount = 0
-                        while ierr != 0 and icount <= ICOUNTMAX:
-                            icount = icount + 1
-                            # The following prints on the sceen the entire
-                            # text of the request
-                            print("Request remote file: ", FILE_REMOTE)
-                            if ((not (os.path.exists(FILE_LOCAL))) or
-                               ((os.path.getsize(FILE_LOCAL)) <= 20000000)):
-                                ierr = download_grb_file(FILE_REMOTE,
-                                                         FILE_LOCAL)
-                                print('dowloading error= ', ierr)
-                                if ierr == 0:  # successeful downloading
-                                    LIST_FLAG[IFILE] = 1
-                                    count_req_files = count_req_files+1
-                                    print(f"Requested remote file downloaded "
-                                          f"in local file{FILE_LOCAL}")
-                                else:  # unsuccesseful downloading
-                                    print(f'Data file {FILE_REMOTE} not '
-                                          f'downloaded! sleep {S_SLEEP2}s')
-                                    time.sleep(S_SLEEP2)
+            ##############################
+            print(f"local_file {local_file}")
+            print(f"remote_file {remote_file}")
+            ############################
 
-                            else:
-                                print('Archivo ya descargado')
-                                ierr = 0
-                                count_req_files = count_req_files + 1
-                                # print count_req_files
+            ierr = 100
+            icount = 0
 
-                        if NFLAG == count_req_files:
-                            WORK = False
-                        if WORK:
-                            print(f"Not all requested files downloaded, "
-                                  f"sleeping {S_SLEEP1} s before next trial")
-                            time.sleep(S_SLEEP1)
-                        else:
-                            print('***************************************'
-                                  '****************')
-                            print(f" All requested grib2 files downloaded !"
-                                  f" {day}, {fci}, UTC")
-                            print('***************************************'
-                                  '****************')
-                            break
-
-                    count = count+1
-                    if WORK:
-                        print(f"All acceptable attempts have been done in this"
-                              f" server, sleeping {S_SLEEP2}s befor request"
-                              f" other server")
+            while ierr != 0 and icount <= ICOUNTMAX:
+                icount = icount + 1
+                # The following prints on the sceen the entire
+                # text of the request
+                print("Request remote file: ", remote_file)
+                if ((not (os.path.exists(local_file))) or ((os.path.getsize(local_file)) <= 20000000)):
+                    # Download de remopte file
+                    ierr = download_grb_file(remote_file, local_file)
+                    print('dowloading error= ', ierr)
+                    if ierr == 0:  # successeful downloading
+                        count_req_files = count_req_files+1
+                        print(f"Requested file downloaded in {local_file}")
+                    else:  # unsuccesseful downloading
+                        print(f'File {remote_file} not downloaded! sleep')
                         time.sleep(S_SLEEP2)
-                    else:
-                        break
+                else:
+                    print('Archivo ya descargado')
+                    ierr = 0
+                    count_req_files = count_req_files + 1
 
-            if not WORK:
-                break
+            if count_down_iter == count_req_files:
+                # if we downloaded all files
+                WORK = False
+        if WORK:
+            print(f"Not all requested files downloaded, "
+                  f"sleeping {S_SLEEP1} s before next trial")
+            time.sleep(S_SLEEP1)
+        else:
+            print('**************************************************')
+            print(f" All requested grib2 files downloaded !")
+            print('**************************************************')
+            break
+
+        count = count+1
+        if WORK:
+            print(f"All acceptable attempts have been done in this"
+                  f" server, sleeping {S_SLEEP2}s befor request"
+                  f" other server")
+            time.sleep(S_SLEEP2)
+        else:
+            break
+
+        if not WORK:
+            break
+
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='descarga_GFS025.py --ini=inidate --output=output',
+        epilog="script de descarga de GFS0925")
 
-    usage = """descarga_GFS025.py --ini=inidate --output=output"""
-    parser = OptionParser(usage)
+    parser.add_argument("--ini", type=int, dest="inidate", help="init date",
+                        required=True)
+    parser.add_argument("--output", dest="output", help="directories where \
+                        downloaded files are stored and (optionally) archived",
+                        required=True)
+    args = parser.parse_args()
 
     # define options
-    parser.add_option("--ini", dest="inidate", help="init date")
-    parser.add_option("--output", dest="output", help="directories where \
-                      downloaded files are stored and (optionally) archived")
+    parser.print_help()
 
-    (opts, args) = parser.parse_args()
-
-    if len(sys.argv) == 1:
-        print(usage)
-    elif not opts.inidate or not opts.output:
+    if not args.inidate or not args.output:
         print("The parameter is required.  Nothing to do!")
-        print(usage)
     else:
-        download(opts.inidate, opts.output)
+        list_remote_files, list_files_local = get_list_gfs(args.inidate)
+        download(args.output, list_remote_files, list_files_local)
 
 
 if __name__ == "__main__":
