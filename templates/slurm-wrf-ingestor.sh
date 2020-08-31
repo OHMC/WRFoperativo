044899757db146279c13235e962b05dbd4aaaaf1
agumartina <amartina@unc.edu.ar>
2020-08-31 09:01:29 -0300

    ver op con WRF 4.1.2

--- templates/slurm-wrf-ingestor.sh
+++ templates/slurm-wrf-ingestor.sh
@@ -23,8 +23,7 @@ conda activate wrfplot-env32
 cd {{TEMP_DIR}}/{{PARAM}}/wrfplot/src
 
 # Token para el usuario wrfplot
-
-export API_BASE_URL_DICT='{"https://ohmc.com.ar/wrf-beta/api": {"token": "29736090019d44b8ee4b52ed54ca25e39b40dabb"}, "https://ohmc.com.ar/wrf/api": {"token": "29736090019d44b8ee4b52ed54ca25e39b40dabb"}}'
+export API_BASE_URL_DICT='{"https://wrf.ohmc.com.ar/api": {"token": "9c0d4c51ef7a43c3eab966b5cc96b549b2496caf"}}'
 
 time python3 api_ingest_csv.py
 
@@ -33,7 +32,7 @@ cd {{TEMP_DIR}}/{{PARAM}}/wrf-cuenca/src
 
 conda activate wrfcuenca3
 
-export API_BASE_URL_DICT='{"https://ohmc.com.ar/wrf-beta/api": {"token": "9c0d4c51ef7a43c3eab966b5cc96b549b2496caf"}, "https://ohmc.com.ar/wrf/api": {"token": "9c0d4c51ef7a43c3eab966b5cc96b549b2496caf"}}'
+export API_BASE_URL_DICT='{"https://wrf.ohmc.com.ar/api": {"token": "9c0d4c51ef7a43c3eab966b5cc96b549b2496caf"}}'
 
 
 python api_ingest_csv.py
